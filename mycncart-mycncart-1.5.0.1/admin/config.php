e75f23aeeb3b24779fa7d95d30f84364f2b9d37f
chenxu <2411233465@qq.com>
2017-03-27 15:37:18 +0800

    提交

---
+++ mycncart-mycncart-1.5.0.1/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/opencart/mycncart-mycncart-1.5.0.1/admin/');
+define('HTTP_CATALOG', 'http://localhost/opencart/mycncart-mycncart-1.5.0.1/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/opencart/mycncart-mycncart-1.5.0.1/admin/');
+define('HTTPS_CATALOG', 'http://localhost/opencart/mycncart-mycncart-1.5.0.1/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/admin/');
+define('DIR_SYSTEM', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/system/');
+define('DIR_IMAGE', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/image/');
+define('DIR_LANGUAGE', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/admin/language/');
+define('DIR_TEMPLATE', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/admin/view/template/');
+define('DIR_CONFIG', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/system/config/');
+define('DIR_CACHE', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/system/storage/cache/');
+define('DIR_DOWNLOAD', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/system/storage/download/');
+define('DIR_LOGS', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/system/storage/logs/');
+define('DIR_MODIFICATION', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/system/storage/modification/');
+define('DIR_UPLOAD', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/system/storage/upload/');
+define('DIR_CATALOG', 'D:/0/wamp64/www/opencart/mycncart-mycncart-1.5.0.1/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'test');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'mcc_');
